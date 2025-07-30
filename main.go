46890b569f0550a8b74ecc8ef037efb7069a40d8
netsensei <matthias@colada.be>
2025-07-30 16:27:03 +0200

    Added: complete error handling during startup

--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/netsensei/bougie/config"
 	"github.com/netsensei/bougie/tui"
 )
 
 func main() {
-	config.Init()
+	err := config.Init()
+	if err != nil {
+		fmt.Println("Error during startup:", err)
+		os.Exit(1)
+	}
+
 	tui.Start()
 }
